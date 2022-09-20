13cf3a825d89da5323d22fb6fa4207bf3b1d04cd
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-09-20 16:16:30 +0200

    Ejercicio Andrea

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (printTimeCallback) {
+         printTimeCallback()
+      }
+      this.currentTime += 1
+    }, 1000)
+    
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    if (value.toString().length == 1) {
+      return "0" + value.toString();
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
+   
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
-  split() {
-    // ... your code goes here
+  split() { 
+    let minuts = this.getMinutes()
+    let seconds = this.getSeconds()
+    let minutsString = this.computeTwoDigitNumber(minuts)
+    let secondsString = this.computeTwoDigitNumber(seconds)
+    
+    return `${minutsString}:${secondsString}`
+    
   }
 }
